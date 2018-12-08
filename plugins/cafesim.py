3ae1afaad29a8059024466364c3eb996d955993b
Matthew Klawitter <mtklaw153@gmail.com>
2018-12-08 13:29:30 -0500

    Finished baseline for cafesim.py

--- plugins/cafesim.py
+++ plugins/cafesim.py
@@ -7,6 +7,7 @@ import threading
 from struct import pack, unpack
 from enum import Enum
 
+from libs.honorbank import HonorBank
 from plugin import Plugin
 from time import sleep
 
@@ -41,21 +42,25 @@ class CafeSim(Plugin):
         self.current_task = None
         # Flag determining if the current action was successfully performed
         self.action_performed = False
+        # Object utilized to save and store a user's score in honor
+        self.bank = HonorBank()
 
         # Launches a deamon thread that handles alerts and random encounters
         thread = threading.Thread(target = self.game_loop)
         thread.daemon = True
         thread.start()
 
+    # Command to assign a user to a role
     def com_role(self, command):
         user = command.user.username
-        role = command.args
+        role_name = command.args
 
-        if Role.has_value(role):
-            self.roles[user] = Role.role
-            return "CafeSim: {} is now a {}!".format(user, role)
-        return "CafeSim: The role '{}' does not exist!".format(role)
+        if Role.has_name(role_name):
+            self.roles[user] = Role.get_role(role_name)
+            return "CafeSim: {} is now a {}!".format(user, role_name)
+        return "CafeSim: The role '{}' does not exist!".format(role_name)
 
+    # Command utilized for users to perform actions
     def com_perform(self, command):
         user = command.user.username
         commands = command.args.split(" ")
@@ -63,7 +68,7 @@ class CafeSim(Plugin):
         if self.current_task == None or self.action_performed:
             return "CafeSim: No services are required at this time."
 
-        if len(commands == 2):
+        if len(commands) == 2:
             if user in self.roles.keys():
                 role = self.roles[user]
 
@@ -75,81 +80,106 @@ class CafeSim(Plugin):
                         return "CafeSim: You're not the correct role for the job! We need a {} to handle this!".format(self.current_task.role.name)
                     return "CafeSim: That's not the requirement we need fulfilled! You need to deal with {}!".format(self.current_task.requirement)
                 return "CafeSim: We don't need that action performed! We are looking for {}!".format(self.current_task.action_name)
-            return "CafeSim: You can join a role with /cs_role [role_name]"
-        return "CafeSim: Invalid syntax - use /cs_perform [action] [requirement]"
+            return "CafeSim: You don't have a role! You can join a role with /csrole [role_name]"
+        return "CafeSim: Invalid syntax - use /csperform [action] [requirement]"
 
+    # Loops and restarts the game based on the var start_time
     def game_loop(self):
+        sleep(10)
         while threading.main_thread().is_alive():
-            start_time = random.randint(10,20) # The amount of time until the next game begins
+            if len(self.channels) > 0:
+                self.game()
+            start_time = random.randint(300,1200) # The amount of time until the next game begins
             sleep(start_time)
-            self.game()
 
+    # Handles the game stack and associated logic
     def game(self):
         action_queue = queue.Queue(10)
         roles = set()
+        reward_amount = 500
+        performance_score = 1.0
 
+        # Froms the set of actions that must be performed during this instance of the game
         for x in range(10):
             action = self.action_manager.pick_action()
             action_queue.put(action)
             roles.add(action.role.name)
 
-        response = "CafeSim: It's time to start a shift!\n The following roles will be necessary:\n"
+        response = "CafeSim: It's time to start a shift!\nThe following roles will be necessary:\n"
 
+        # Forms the neccessary set of roles required to beat the game
         for role in roles:
-            response += "{}".format(role)
+            response += "{}\n".format(role)
 
+        # Messages chat channels the game's requirements
         self.message_channels(response)
         sleep(10)
-
         self.message_channels("CafeSim: Here we go!")
+        sleep(2)
 
+        # Runs until all actions have left the action_queue Queue 
         while not action_queue.empty():
             action = action_queue.get()
             requirement = random.choice(action.requirements)
             role = action.role
 
+            # Sets the current action that must quickly be performed
             self.action_performed = False
-            self.current_task = Task(action.name, requirement, role)
-            self.message_channels(action.trigger_message + "\n (A {} must perform:\n /cs_perform {} {})".format(role.name, action.name, requirement))
+            self.current_task = Task(action.action_name, requirement, role)
+            self.message_channels(action.trigger_message + "\n(A {} must perform:\n/csperform {} {})".format(role.name, action.action_name, requirement))
             sleep(8)
 
+            # Determines if the action was performed. If not, then the score is penalized. 
             if self.action_performed:
                 self.message_channels("Very good work! Our customers are happy!")
             else:
                 self.message_channels("Terrible work! Your pay's been docked!")
+                performance_score -= .1
 
+            # Resets current task and bool flag for the next action
             self.current_task = None
             self.action_performed = False
-            sleep(5)
+            sleep(3)
 
+        # End of the game logic, rewards score and messages based on performance
+        reward = int(reward_amount * performance_score)
         self.message_channels("Good work everyone, the shift is nowover! Time to get paid! See you again soon!")
+        self.message_channels("CafeSim:\n Performance score: {}\n Paying out {} honor to those that helped!".format(str(round(performance_score, 2)), str(reward)))
 
+        for user in self.roles.keys():
+            if self.bank.account_exists(user):
+                self.bank.pay(user, reward)
+            else:
+                self.bank.create_account(user)
+                self.bank.pay(user, reward)
+
+    # Helper method that messages all channels within self.channels
     def message_channels(self, message):
         for channel in self.channels:
             self.bot.send_message(channel, message)
 
     # Run whenever someone on telegram types one of these commands
     def on_command(self, command):
-        if command.command == "cs_enable":
+        if command.command == "csenable":
             if command.chat.id in self.channels:
                 return {"type": "message", "message": "This Cafe is already open for business."}
             else:
                 self.channels.add(command.chat.id)
                 return {"type": "message", "message": "The Cafe is now open."}
-        elif command.command == "cs_disable":
+        elif command.command == "csdisable":
             if command.chat.id in self.channels:
                 self.channels.remove(command.chat.id)
                 return {"type": "message", "message": "The Cafe is now close."}
             else:
                 return {"type": "message", "message": "This Cafe has not been opened for business."}
-        elif command.command == "cs_role":
+        elif command.command == "csrole":
             return {"type": "message", "message": self.com_role(command)}
-        elif command.command == "cs_perform":
+        elif command.command == "csperform":
             return {"type": "message", "message": self.com_perform(command)}
 
     # Commands that are enabled on the server. These are what triggers actions on this plugin
     def get_commands(self):
-        return {"cs_enable", "cs_disable", "cs_role", "cs_perform"}
+        return {"csenable", "csdisable", "csrole", "csperform"}
 
     # Returns the name of the plugin
     def get_name(self):
@@ -157,10 +187,10 @@ class CafeSim(Plugin):
 
     # Run whenever someone types /help cafesim
     def get_help(self):
-        return "'/cs_enable' to enable the game in this channel \n,\
-                '/cs_disable' to disable the game in this channel\n,\
-                '/cs_role [role_name]' to become a role\n,\
-                '/cs_perform [action] [requirement]' to perfrom an action"
+        return "'/csenable' to enable the game in this channel \n,\
+                '/csdisable' to disable the game in this channel\n,\
+                '/csrole [role_name]' to become a role\n,\
+                '/csperform [action] [requirement]' to perfrom an action"
 
 
 # Wrapper for holding the next action to be performed by a player
@@ -178,18 +208,18 @@ class ActionManager:
     def build_actions(self):
         return {
             "wait" : Action("wait", "A customer is ready to order! Go wait on them!", ["John", "Jessie", "James", "Jackie"], Role.server),
-            "collect" : Action("collect", "Some food is ready to be served, go deliver it!", ["pie", "eggs", "pancakes", "turkey_club"], Role.server),
+            "deliver" : Action("deliver", "Some food is ready to be served, go deliver it!", ["pie", "eggs", "pancakes", "turkey_club"], Role.server),
             "charge" : Action("charge", "A customer is ready to pay!", ["John", "Jessie", "James", "Jackie"], Role.server),
             "brew" : Action("brew", "The customer wants a coffee! Go brew one!", ["espresso", "mocha"], Role.barista),
             "grind" : Action("grind", "Some coffee beans need grinding!", ["mocha", "arabica"], Role.barista),
             "mix" : Action("mix", "A specialty drink needs to be mixed!", ["double_expresso", "pumpkin_latte"], Role.barista),
             "clean" : Action("clean", "There's a mess on the floor! Go clean it!", ["spill", "trash"], Role.cleaner),
             "scrub" : Action("scrub", "Stuff is covered in grease and needs scrubbing!", ["grill", "counter"], Role.cleaner),
-            "dust" : Action("dust", "There's dust everywhere and it needs cleaning!", ["walls", "windows"], Role.cleaner),
+            "dust" : Action("dust", "There's dust everywhere and it needs cleaning!", ["walls", "windows"], Role.cleaner)
         }
         
     def pick_action(self):
-        key = random.choice(self.actions.keys())
+        key = random.choice(list(self.actions.keys()))
         return self.actions[key]
 
     def action_exists(self, action_name):
@@ -213,5 +243,15 @@ class Role(Enum):
     cleaner = 2
 
     @classmethod
-    def has_value(Role, value):
-        return any(value == item.value for item in Role)
+    def has_name(Role, name):
+        for item in Role:
+            if name == item.name:
+                return True
+        return False
+
+    @classmethod
+    def get_role(Role, name):
+        for item in Role:
+            if item.name == name:
+                return item
+        return None

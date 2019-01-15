e8ad3e4e7c42e7512d8d43d980cc2875cc815161
Matthew Klawitter <mtklaw153@gmail.com>
2019-01-14 21:38:52 -0500

    Added /mcplayers to view player names connected

--- plugins/minecraftstatus.py
+++ plugins/minecraftstatus.py
@@ -29,18 +29,25 @@ class MinecraftStatus(Plugin):
     def get_ping(self):
         return "The server responded in {}ms".format(self.server.ping())
 
+    def get_players(self):
+        query = self.server.query()
+        return "The following players are connected: {}".format(", ".join(query.players.names))
+
     def on_command(self, command):
         if command.command == "mcstatus":
             return {"type": "message", "message": self.get_status()}
         elif command.command == "mcping":
             return {"type": "message", "message": self.get_ping()}
+        elif command.command == "mcplayers":
+            return {"type": "message", "message": self.get_players()}
 
     def get_commands(self):
-        return {"mcstatus", "mcping"}
+        return {"mcstatus", "mcping", "mcplayers"}
 
     def get_name(self):
         return "Minecraft Status"
 
     def get_help(self):
         return "'/mcstatus' to see how many players are currently connected\n \
-                '/mcping' to see the server's ping"
+                '/mcping' to see the server's ping\n \
+                '/mcplayers' to see player names connected"

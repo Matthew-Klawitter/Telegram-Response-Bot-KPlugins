e1200dbfc0b032344c8384b387f2d1a9bd812347
Matthew Klawitter <mtklaw153@gmail.com>
2019-02-27 15:16:54 -0500

    Added command to configure server ip

--- plugins/minecraftstatus.py
+++ plugins/minecraftstatus.py
@@ -9,7 +9,7 @@ def load(data_dir, bot):
 
 """
 Created by Matthew Klawitter 1/9/2019
-Last Updated: 1/9/2019
+Last Updated: 2/27/2019
 Version: v1.0.0.0
 """
 
@@ -18,27 +18,37 @@ class MinecraftStatus(Plugin):
     def __init__(self, data_dir, bot):
         self.data_dir = data_dir
         self.bot = bot
-        host = "mc.marks.kitchen"
-        self.server = MinecraftServer.lookup(host)
+        self.is_setup = False
+        self.server = None
 
+    def setup(self, command):
+        commands = command.args.split(" ")
+        self.host = commands[0]
+        self.server = MinecraftServer.lookup(host)
+        self.is_setup = True
 
     def get_status(self):
         status = self.server.status()
-        return "There are currently {} players connected.".format(status.players.online)
+        return "MCStatus: There are currently {} players connected.".format(status.players.online)
 
     def get_ping(self):
-        return "The server responded in {}ms".format(self.server.ping())
+        return "MCStatus: The server responded in {}ms".format(self.server.ping())
 
     def get_players(self):
         status = self.server.status()
-        response = "The following players are connected:\n"
+        response = "MCStatus: The following players are connected:\n"
 
         for player in status.players.sample:
             response += player.name + "\n"
         return response
 
     def on_command(self, command):
-        if command.command == "mcstatus":
+        if command.command == "mcsetup":
+            return {"type": "message", "message": self.setup(command)}
+
+        if not self.is_setup():
+            return {"type": "message", "message": "MCStatus: Please first run /mcsetup [ip] to configure a server."}
+        elif command.command == "mcstatus":
             return {"type": "message", "message": self.get_status()}
         elif command.command == "mcping":
             return {"type": "message", "message": self.get_ping()}
@@ -46,7 +56,7 @@ class MinecraftStatus(Plugin):
             return {"type": "message", "message": self.get_players()}
 
     def get_commands(self):
-        return {"mcstatus", "mcping", "mcplayers"}
+        return {"mcsetup", "mcstatus", "mcping", "mcplayers"}
 
     def get_name(self):
         return "Minecraft Status"
@@ -54,4 +64,5 @@ class MinecraftStatus(Plugin):
     def get_help(self):
         return "'/mcstatus' to see how many players are currently connected\n \
                 '/mcping' to see the server's ping\n \
-                '/mcplayers' to see player names connected"
+                '/mcplayers' to see player names connected\n \
+                '/mcsetup [ip]' to configure which server to obtain information on."
